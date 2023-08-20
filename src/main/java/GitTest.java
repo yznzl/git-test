ea13fbb98dc4a193be8c1e6aea407d4224373d6e
yznzl <142674368+yznzl@users.noreply.github.com>
2023-08-20 20:48:04 +0800

    pull test

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("hello master");
         System.out.println("hello hotfix");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
